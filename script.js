ae09b942c7c52770ceb20b7623aee9eafbe737de
horseywin <horsey2019@duck.com>
2025-01-24 15:13:34 +1000

    Addded popup stability and ease

--- script.js
+++ script.js
@@ -6,15 +6,27 @@ function applyEventListeners(projectDivId, popupDivName) {
     });
 }
 
-function addPopUp(title, description, imageURL, link, popupDivName, projectDivId) {
+function addProject(title, description, imageURLList, popupDivName, projectDivId) {
     const BodyTag = document.getElementsByTagName("body")[0];
+    const projectDiv = document.getElementById("projects");
+    let imageInsertionCode = ""
+    
+    imageURLList.forEach((imageURL) => {
+        imageInsertionCode += `<div style="${"background-image: url(" + imageURL + ");"}" class="popup-image"></div>`
+    })
+    projectDiv.innerHTML += `
+    <div class="project-tile" id=${projectDivId} style="${"background-image: url(" + imageURLList[0] + ");"}">
+        <h2 class="project-title">${title}</h2>
+    </div>
+    `;
     BodyTag.innerHTML += `
         <div id="${popupDivName}" class="popupDiv" style="display: none;">
             <div class="popup");">
                 <h1 class="popup-title">${title}</h1>
                 <h3 class="popup-description">${description}</h3>
-                <a class="popup-description" href="${link}" style="text-decoration: underline;">Github project here</a>
-                <img src="${imageURL}">
+                <div class="popup-image-container">
+                    ${imageInsertionCode}
+                </div>
             </div>
             <div class="popup-exit"></div>
         </div>
@@ -25,7 +37,7 @@ function addPopUp(title, description, imageURL, link, popupDivName, projectDivId
 //THIS IS START OF WHERE YOU EDIT
 
 // Tetris
-addPopUp("Example", "Examples are the backbone of understanding and learning, providing a clear and tangible way to grasp complex concepts. They bridge the gap between theory and practice, showing how ideas work in real-world scenarios. Without examples, even the most well-explained ideas can feel abstract and confusing. Whether you’re coding, solving a math problem, or learning a new skill, examples give you a foundation to build upon. They also inspire creativity by showcasing what’s possible, helping you apply the knowledge to your unique needs. In short, examples aren’t just helpful—they’re essential.", "Tetris-Game-Coding.png", "https://github.com/horseywin/Tetris-Game", "tetris-popup", "tetris-game");
+addProject("Example", "Examples are the backbone of understanding and learning, providing a clear and tangible way to grasp complex concepts. They bridge the gap between theory and practice, showing how ideas work in real-world scenarios. Without examples, even the most well-explained ideas can feel abstract and confusing. Whether you’re coding, solving a math problem, or learning a new skill, examples give you a foundation to build upon. They also inspire creativity by showcasing what’s possible, helping you apply the knowledge to your unique needs. In short, examples aren’t just helpful—they’re essential.", ["example.jpeg", "example2.webp", "example3.avif"], "tetris-popup", "tetris-game");
 
 
 
